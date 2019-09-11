94262e6527782b55e49d04ad163c56996f6e4234
danieldaniel9292 <danielweber1992@outlook.de>
2019-09-11 21:32:06 +0200

    changes bootstrap app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {SharedModule} from './shared/shared.module';
+import {RoutingModule} from './routing.module';
+import { DashboardComponent } from './application/dashboard/dashboard.component';
+import { ApplicationsComponent } from './application/applications/applications.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ApplicationsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
